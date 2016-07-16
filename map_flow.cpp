0e5027d5162c455de145cce3c03a78fbd95a4cfc
AmirooR <noname01.cpp@gmail.com>
2016-07-16 14:45:01 +0430

    write from pixel with max flow value to pixels seen more than once!

--- map_flow.cpp
+++ map_flow.cpp
@@ -49,22 +49,51 @@ Mat read_flow(const char *filename)
 
 Mat fill_naive_img2(Mat img1, Mat flow)
 {
+    int* seen = new int[img1.rows*img1.cols];
+    float* max_seen_flow_x = new float[img1.rows*img1.cols];
+    float* max_seen_flow_y = new float[img1.rows*img1.cols];
+
+    for(int i=0;i<img1.rows*img1.cols; i++) {seen[i] = 0;max_seen_flow_x[i] = 0;max_seen_flow_y[i] = 0;}
+
     Mat img2_n(img1.rows, img1.cols, CV_8UC3);
     img2_n = Scalar(0, 255, 0);
     for(int r = 0; r < img1.rows; r++)
     {
         for(int c = 0; c < img1.cols; c++)
         {
-            Point2f f = flow.at<Point2f>(r,c);
-            f.x = f.x+c;
-            f.y = f.y+r;
+            Point2f f1 = flow.at<Point2f>(r,c);
+            Point2f f;
+            f.x = floor(f1.x+c+0.5);
+            f.y = floor(f1.y+r+0.5);
             if( f.x >=0 && f.y >=0 && f.x < img1.cols && f.y < img1.rows)
             {
-                img2_n.at<Vec3b>( (int)floor( f.y + 0.5) , (int)floor(f.x + 0.5)) = img1.at<Vec3b>(r,c);
+                int idx = (int) f.x + f.y*img1.cols;
+                seen[idx]++;
+                if(seen[idx] >= 2)
+                {
+                    float this_flow = abs(f1.x) + abs(f1.y);
+                    if( this_flow > abs(max_seen_flow_x[idx]) + abs(max_seen_flow_y[idx]) )
+                    {
+                        max_seen_flow_x[idx] = f1.x;
+                        max_seen_flow_y[idx] = f1.y;
+                    }// r,c = f - f1
+                    int new_r = (int) f.y - max_seen_flow_y[idx];
+                    int new_c = (int) f.x - max_seen_flow_x[idx];
+                    img2_n.at<Vec3b>( (int)f.y , (int)f.x) = img1.at<Vec3b>(new_r,new_c);
+                }
+                else
+                {
+                    img2_n.at<Vec3b>( (int)f.y , (int)f.x) = img1.at<Vec3b>(r,c);
+                    max_seen_flow_x[idx] = f1.x;
+                    max_seen_flow_y[idx] = f1.y;
+                }
             }
             
         }
     }
+    delete[] seen;
+    delete[] max_seen_flow_y;
+    delete[] max_seen_flow_x;
     return img2_n;
 }
 
