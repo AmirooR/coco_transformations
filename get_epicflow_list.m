0b13ed2ed092105867f96a5b663a0f9355711e1d
AmirooR <noname01.cpp@gmail.com>
2016-08-07 02:03:25 +0430

    epic flow on list added

---
+++ get_epicflow_list.m
@@ -0,0 +1,6 @@
+function get_epicflow_list(img1_path_list, img2_path_list, flow_path_list)
+    for i=1:length(img1_path_list)
+        img1 = imread(img1_path_list(i))
+        img2 = imread(img2_path_list(i))
+        get_epicflow(img1, img2, flow_path_list(i), i)
+    end
