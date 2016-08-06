17f4421c85e22a9cad30a42beec663db12378462
amiroor <noname01.cpp@gmail.com>
2016-08-06 18:23:00 -0400

    compute/get epicflow fixes

--- compute_epicflow.py
+++ compute_epicflow.py
@@ -1,6 +1,7 @@
 import util
 import os.path as osp
 from davis import cfg
+import matlab.engine
 
 def get_epicflow_input_lists(split):
     sequences = util.load_davis_sequences(split)
@@ -10,8 +11,16 @@ def get_epicflow_input_lists(split):
     for sequence in sequences:
         orig_name = sequence['name']
         for frame_id in xrange(sequence['num_frames'] - 1):
-            img1_list.append( cfg.PATH.SEQUENCES_DIR, orig_name, '%05d.jpg' % frame_id)
-            img2_list.append( cfg.PATH.SEQUENCES_DIR, orig_name, '%05d.jpg' % frame_id+1)
-            flow_list.append( cfg.PATH.ANNOTATION_DIR, orig_name, '%05d_inv.flo' % frame_id+1)
+            img1_list.append( osp.join(cfg.PATH.SEQUENCES_DIR, orig_name, '%05d.jpg' % frame_id))
+            img2_list.append( osp.join(cfg.PATH.SEQUENCES_DIR, orig_name, '%05d.jpg' % (frame_id+1)))
+            flow_list.append( osp.join(cfg.PATH.ANNOTATION_DIR, orig_name, '%05d_inv.flo' % (frame_id+1)))
 
     return img1_list, img2_list, flow_list
+
+
+
+if __name__ == '__main__':
+    split = 'test'
+    eng = matlab.engine.start_matlab()
+    img1_list,img2_list, flow_list = get_epicflow_input_lists(split)
+    eng.get_epicflow_list( img2_list, img1_list, flow_list)
